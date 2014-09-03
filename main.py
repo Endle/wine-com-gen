49c2deaa8e39b1472c068b2c97ca05be51b2652e
Zhenbo Li <litimetal@gmail.com>
2014-09-03 22:31:42 +0800

    First step: generate header

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
 
+TRACE_FLAG = True
+def TRACE(s):
+    if TRACE_FLAG:
+        print(s)
+
+def replace_function_type(s):
+    return s.replace("FORCEINLINE HRESULT I", "HRESULT WINAPI ")
+
 def single_function(s):
     '''
     Receives a string for a function
     Returns a string for a function'''
-    return None
+
+    s1 = replace_function_type(s)
+    s1 = s1.replace('This', 'iface')
+
+#All necessary imformation is in header
+    header = s1.split('{')[0].rstrip()
+
