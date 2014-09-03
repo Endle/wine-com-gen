2928604c4f934395f3643fb5d63921ac1b6020bb
Zhenbo Li <litimetal@gmail.com>
2014-09-03 22:37:30 +0800

    test: expect a space after ','

--- tests/TestTable.py
+++ tests/TestTable.py
@@ -4,7 +4,7 @@ TABLE_SINGLE_FUNCTION = [
 ('''static FORCEINLINE HRESULT IHTMLXMLHttpRequest_get_responseXML(IHTMLXMLHttpRequest* This,IDispatch **p) {
     return This->lpVtbl->get_responseXML(This,p);
 }''',
-'''static HRESULT WINAPI HTMLXMLHttpRequest_get_responseXML(IHTMLXMLHttpRequest* This,IDispatch **p)
+'''static HRESULT WINAPI HTMLXMLHttpRequest_get_responseXML(IHTMLXMLHttpRequest* This, IDispatch **p)
 {
     HTMLXMLHttpRequest *This = impl_from_IHTMLXMLHttpRequest(iface);
     FIXME("(%p)->(%p)\n", This, p);
