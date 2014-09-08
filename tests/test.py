504616e1fbb0b6a15cf4710bebac12b1e6ae569a
Zhenbo Li <litimetal@gmail.com>
2014-09-08 14:37:13 +0800

    test: new helper '_test_table' to make the code cleaner

--- tests/test.py
+++ tests/test.py
@@ -7,9 +7,11 @@ import main
 import TestTable
 
 class ConvertTestCase(unittest.TestCase):
+    def _test_table(self, func, table):
+        for case in table:
+            self.assertEqual(func(case[0]), case[1])
     def test_single_function(self):
-        for case in TestTable.TABLE_SINGLE_FUNCTION:
-            self.assertEqual(main.single_function(case[0]), case[1])
+        self._test_table(main.single_function, TestTable.TABLE_SINGLE_FUNCTION)
 
 def suite():
     suite = unittest.TestSuite()
